69dc596be9a54faed2122a3c089be61f5b28a730
yytina <yang.hyejin@gmail.com>
2013-10-11 12:32:40 +0800

    Initial Commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('referenceMeApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
